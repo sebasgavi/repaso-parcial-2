4fb2f25b88f57f17074e495cb455772774fa4e2d
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2018-11-14 08:03:00 -0500

    instrucciones

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+// importar módulo express (se debe instalar con npm)
 const express = require('express');
+
+// importar módulo download (se debe instalar con npm)
 const download = require('download');
+
+// importar módulo filesystem (ya viene instalado con node)
 const fs = require('fs');
 
+// crear el servidor
 const app = express();
 
 /**
  * Formulario para enviar las variables (sin fetch)
  */
 app.get('/', function(request, response){
+    // Se utiliza el comando send para enviar el HTML
     response.send(`
         <form method="GET" action="/api/download">
             <input name="img" placeholder="image url">
@@ -21,6 +28,7 @@ app.get('/', function(request, response){
  * Formulario para enviar las variables (con fetch)
  */
 app.get('/fetch', function(request, response){
+    // Se utiliza el comando send para enviar el HTML y JS
     response.send(`
         <form>
             <input name="img" placeholder="image url">
@@ -47,10 +55,15 @@ app.get('/fetch', function(request, response){
  * name: nombre del archivo
  */
 app.get('/api/download', function(request, response){
+    // Con el módulo download descarga la imagen con la ruta en la variable img
     download(request.query.img).then(data => {
+        // Con el módulo fs se guarda la imagen en la carpeta images con el nombre de la variable name
+        // Es importante que la carpeta images exista previamente
         fs.writeFileSync('images/'+request.query.name+'.jpg', data);
+        // Se envía un mensaje al usuario después de la descarga
+        response.send('saved: ' + request.query.name);
     });   
-    response.send('saved: ' + request.query.name);
 });
 
+// iniciar el servidor
 app.listen(5500);
